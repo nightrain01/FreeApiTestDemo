1a2c4c4e666c94420c937e33a2338e024eb0b5cf
nightrain01 <nightrain.h@qq.com>
2024-04-10 12:31:57 +0800

    删除多余的一些内容，优化变量逻辑

--- util/read_data.py
+++ util/read_data.py
@@ -4,12 +4,6 @@ from pprint import pprint
 import yaml
 
 
-def read_yaml_data(path):
-    with open(path, 'r', encoding='utf-8') as f:
-        data = yaml.safe_load(f)
-    return data
-
-
 class DataReader:
     @staticmethod
     def read_yaml_data(path):
@@ -22,14 +16,14 @@ class DataReader:
 
 
 class DataGetter:
-    DATA_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    reader = DataReader()
+    DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
+    dataReader = DataReader()
 
     @classmethod
     def get_parametrize_data(cls):
-        yaml_data_path = os.path.join(cls.DATA_PATH, 'data', 'test_parametrize_data.yml')
+        yaml_data_path = os.path.join(cls.DATA_PATH, 'test_parametrize_data.yml')
         try:
-            parametrize_data = cls.reader.read_yaml_data(yaml_data_path)
+            parametrize_data = cls.dataReader.read_yaml_data(yaml_data_path)
             return parametrize_data
         except Exception as e:
             print(e)
